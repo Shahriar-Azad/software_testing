750b0b9039b8fe54f26505a04b461348f90c2f36
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-08-20 23:28:50 +0600

    first add

---
+++ hello.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+# driver = webdriver.Chrome()
+# driver.get("https://opensource-demo.orangehrmlive.com/")
+
+# try:
+#     WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located((By.NAME, "username"))
+#     )
+
+#     driver.find_element(By.NAME, "username").send_keys("Admin")
+#     driver.find_element(By.NAME, "password").send_keys("admin123")
+#     driver.find_element(By.XPATH, "//button[@type='submit']").click()
+
+#     # check title
+#     actual_title = driver.title
+#     expected_title = "OrangeHRM"
+#     if actual_title == expected_title:
+#         print("Login test passed!")
+#     else:
+#         print("Login test failed!")
+
+# finally:
+#     input("Enter to close the browser")
+#     driver.quit()

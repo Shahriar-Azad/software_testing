bd34e487ee8ea46ff09566d0331630407ec2af25
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-08-20 23:32:21 +0600

    added more

--- hello.py
+++ hello.py
@@ -13,3 +13,8 @@ try:
         EC.presence_of_element_located((By.NAME, "username"))
     )
 
+    driver.find_element(By.NAME, "username").send_keys("Admin")
+    driver.find_element(By.NAME, "password").send_keys("admin123")
+    driver.find_element(By.XPATH, "//button[@type='submit']").click()
+
+
